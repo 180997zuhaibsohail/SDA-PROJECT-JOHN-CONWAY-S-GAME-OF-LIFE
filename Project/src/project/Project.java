fdad56205a6ec117d5871525b4cc8495c43c4632
ZUHAIB SOHAIL <ZUHAIB SOHAIL@DESKTOP-HOI82AF>
2021-11-23 20:41:01 +0500

    some functionalities added

---
+++ Project/src/project/Project.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author ZUHAIB SOHAIL
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameOfLife life = new GameOfLife();
+        life.setLocationRelativeTo(null);
+        life.show();
+    }
+    
+}
